9bb1a3e084ba6acf6e25998dfc10252585eb13ca
ethanschwartz <e.schwartz@seclock.com>
2022-06-27 13:07:48 +0300

    - removed one unused line import

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
-use Inertia\LazyProp;
 use Inertia\Response;
 
 class SubscriberController extends Controller
